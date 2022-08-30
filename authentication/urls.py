20df14d6517b1901e7864ec047c7414af182763e
vwnx1 <vwnx123@gmail.com>
2022-08-30 13:48:50 -0300

    Adicionadas as URLS da app

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
+from . import views
 
+app_name = 'authentication'
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('login', views.login_user, name='login'),
+    path('success', views.login_user, name='success'),
+    path('logout_user', views.logout_user, name='logout_user'),
+    path('register_user', views.register_user, name='register_user')
+]
